394044dbffe9834fc371103304a4aff97d185c08
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-04 14:32:16 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] cibi = { "pizza", "pasta", "kebab", "panino", "piadina" };
+
+Console.WriteLine($"Lunghezza classifica: {cibi.Length}");
+
+Console.WriteLine("Classifica:");
+for  ( int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine($"{i + 1}) {cibi[i]}");
+}
+
+Console.WriteLine($"Cibo top: {cibi[0]}");
+
+Console.WriteLine($"Cibo meno preferito: {cibi[cibi.Length - 1]}");
