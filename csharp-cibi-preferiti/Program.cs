6d248148261c3495204bc22d46ec9012b6409e4d
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-04 14:44:10 +0200

    Aggiunta classifica cibo di mezzo

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -11,3 +11,14 @@ for  ( int i = 0; i < cibi.Length; i++)
 Console.WriteLine($"Cibo top: {cibi[0]}");
 
 Console.WriteLine($"Cibo meno preferito: {cibi[cibi.Length - 1]}");
+
+int indiceMedio = cibi.Length / 2;
+
+if(cibi.Length % 2 == 0)
+{
+    Console.WriteLine($"Cibo di mezzo: {cibi[indiceMedio]} e {cibi[indiceMedio - 1]}");
+}
+else
+{
+    Console.WriteLine($"Cibo di mezzo: {cibi[indiceMedio]}");
+}
